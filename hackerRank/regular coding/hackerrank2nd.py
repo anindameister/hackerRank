99b0a9ecc0a1adfbc8609a15f87c4069dad9884c
anindameister <anindameister0@gmail.com>
2020-05-23 02:08:00 +0200

    2nd day

---
+++ hackerRank/regular coding/hackerrank2nd.py
@@ -0,0 +1,14 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input()) 
+print(i+a)
+print(d+b)
+print(s+c)
